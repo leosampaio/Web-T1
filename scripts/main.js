0a80ab95bb71d8171986b0e29ca8b19316b046ce
Lais Helena Chiachio de Miranda <lalena.chiachio@gmail.com>
2017-05-20 23:21:27 -0300

    Include events logout and profile to navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function() {
+
     document.addEventListener('did-click-cart', function(e) {
         console.info('Got a did-click-cart on main.js');
         window.location.replace("client-cart.html");
     });
 
-    document.addEventListener('did-select-pet', function(e) {
+    document.addEventListener('did-click-profile', function(e){
+    	console.info('Got a did-click-profile on main.js');
+        // window.location.replace("#"); //TODO
+    });
+
+    document.addEventListener('did-click-logout', function(e){
+    	console.info('Got a did-click-logout on main.js');
+        window.location.replace("adm-login.html"); 
+    });
+
+	document.addEventListener('did-select-pet', function(e) {
         console.info('Got a did-select-pet with pet: ', e.detail.pet);
         window.location.replace("client-edit-pet.html");
     });
-})
+});
