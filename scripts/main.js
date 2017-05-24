f473f06c1ff61ebabc18bf3656f3d4a832ab9c8a
Leonardo Sampaio Ferraz Ribeiro <leo.sampaio.ferraz.ribeiro@gmail.com>
2017-05-23 22:47:56 -0300

    Fixed sidebar events and removed hre's from links

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.replace("client-cart.html");
     });
 
-    document.addEventListener('did-click-profile', function(e){
+    document.addEventListener('did-click-profile', function(e) {
     	console.info('Got a did-click-profile on main.js');
         // window.location.replace("#"); //TODO
     });
 
-    document.addEventListener('did-click-logout', function(e){
+    document.addEventListener('did-click-logout', function(e) {
     	console.info('Got a did-click-logout on main.js');
         window.location.replace("adm-login.html"); 
     });
@@ -21,10 +21,49 @@ document.addEventListener("DOMContentLoaded", function() {
         console.info('Got a did-select-pet with pet: ', e.detail.pet);
         window.location.replace("client-edit-pet.html");
     });
-    document.addEventListener('did-click-logout', function(e){
-    	console.info('Got a did-click-logout on main.js');
-        window.location.replace("adm-login.html"); 
+
+    document.addEventListener('did-select-admins', function(e) {
+        var event = new CustomEvent('did-select-admins on main.js');
+        window.location.replace("adm-adms.html");
     });
 
+    document.addEventListener('did-select-clients', function(e) {
+        console.info('Got a did-select-clients on main.js');
+        window.location.replace("adm-clients.html");
+    });
 
+    document.addEventListener('did-select-products', function(e) {
+        console.info('Got a did-select-products on main.js');
+        window.location.replace("adm-products.html");
+    });
+
+    document.addEventListener('did-select-earnings', function(e) {
+        console.info('Got a did-select-earnings on main.js');
+        window.location.replace("adm-earnings.html");
+    });
+
+    document.addEventListener('did-select-sales', function(e) {
+        console.info('Got a did-select-sales on main.js');
+        window.location.replace("adm-sales.html");
+    });
+
+    document.addEventListener('did-select-calendar', function(e) {
+        console.info('Got a did-select-calendar on main.js');
+        window.location.replace("adm-calendar.html");
+    });
+
+    document.addEventListener('client-did-select-pets', function(e) {
+        console.info('Got a client-did-select-pets on main.js');
+        window.location.replace("client-pets.html");
+    });
+
+    document.addEventListener('client-did-select-calendar', function(e) {
+        console.info('Got a client-did-select-calendar on main.js');
+        window.location.replace("client-calendar.html");
+    });
+
+    document.addEventListener('client-did-select-products', function(e) {
+        console.info('Got a client-did-select-products on main.js');
+        window.location.replace("client-products.html");
+    });
 });
