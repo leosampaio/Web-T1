5a794408c3b0a79d6fe25e23aef1e2d3a6e9efff
Leonardo Sampaio Ferraz Ribeiro <leo.sampaio.ferraz.ribeiro@gmail.com>
2017-07-03 23:12:24 -0300

    Implement API setup, list and get for Admin database

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
+let nano = require('nano')('http://localhost:5984');
 
-router.get('/', function(req, res, next) {
-  res.json({
-    foo: "bar",
-    baz: "quux"
-  });
+let Admin = require('./models_api/admin.js');
+
+router.get('/admins', (req, res) => {
+    Admin.getAll().then((response) => {
+        res.json(response);
+    }).catch((error) => {
+        console.log(error);
+        res.json(error);
+    })
+});
+
+router.get('/admins/:id', (req, res) => {
+    Admin.getByID(req.params.id).then((response) => {
+        res.json(response);
+    }).catch((error) => {
+        console.log(error);
+        res.send(error);
+    })
 });
 
 module.exports = router;
