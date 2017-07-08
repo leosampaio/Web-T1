6a4d436e8e7ebc4f0196131880034c936679ebb8
Sabrina <sabrina.tridico@usp.br>
2017-07-07 23:48:51 -0300

    Models updated

--- api/models_api/product.js
+++ api/models_api/product.js
@@ -1,7 +1,74 @@
 'use strict';
 
 let Product = require('../models/product.js');
+let nano = require('nano')('http://localhost:5984');
 
 class ProductServer extends Product {
+	static getAll() {
+        let p = new Promise((resolve, reject) => {
+            let ProductDB = nano.use('product');
+            ProductDB.view('products', 'by_id', {
+                'include_docs': true
+            }, (err, body) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                    return;
+                } else {
+                    let models = [];
+                    body.rows.forEach((doc) => {
+                        let model = new Product(doc.doc);
+                        models.push(model);
+                    });
+                    resolve(models);
+                }
+            });
+        });
+        return p;
+    }
 
+    static getByID(id) {
+        let p = new Promise((resolve, reject) => {
+            let ProductDB = nano.use('product');
+            ProductDB.view('products', 'by_id', {
+                'key': parseInt(id),
+                'include_docs': true
+            }, (err, body) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                } else {
+                    if (body.rows.length > 0) {
+                        let model = new Product(body.rows[0].doc);
+                        resolve(model);
+                    } else {
+                        reject(404);
+                    }
+                }
+            });
+        });
+        return p;
+    }
+
+    static insert(model) {
+        let p = new Promise((resolve, reject) => {
+            let ProductDB = nano.use('product');
+            ProductDB.insert(model, function(err, body) {
+                if (!err) {
+                    console.log(body);
+                    resolve(model);
+                } else {
+                    reject(500);
+                }
+            });
+        });
+        return p;
+    }
+
+    static delete(id) {
+        let p = new Promise((resolve, reject) => {
+            
+        });
+        return p;
+    }
 }
